3ca253c257c9aaed94bac7b333268614c0ce9f84
Tommy Danks <tommydanks2000@hotmail.com>
2023-05-01 02:18:44 +0100

    ping pong

--- src/commands/Information/ping.js
+++ src/commands/Information/ping.js
@@ -1,14 +1,50 @@
-const { SlashCommandBuilder } = require("discord.js");
+const dayjs = require("dayjs");
+const duration = require("dayjs/plugin/duration");
+const relativeTime = require("dayjs/plugin/relativeTime");
+
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!"),
   async execute(interaction, client) {
-    await interaction.reply(
-      `🏓 Latency is ${
-        Date.now() - new Date(interaction.createdTimestamp)
-      }ms. \n    API Latency is ${Math.round(client.ws.ping)}ms`
-    );
+    const data = await client.pingStats();
+    const uptime = dayjs.duration(client.uptime).humanize();
+
+    const embed = new EmbedBuilder()
+      .setTitle("🏓 PONG!")
+      .setColor(client.color)
+      .addFields(
+        {
+          name: "Api Latency",
+          value: `${data.apiLatency}ms`,
+          inline: true,
+        },
+        {
+          name: "Client Latency",
+          value: `${Date.now() - new Date(interaction.createdTimestamp)}ms`,
+          inline: true,
+        },
+        {
+          name: "Memory Usage",
+          value: `${client.toFixedNumber(data.memoryUsage)}mb`,
+          inline: true,
+        }
+      )
+      .setFooter({
+        text: `Up for  ${uptime}`,
+      })
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [embed] });
+
+    // await interaction.reply(
+    //   `🏓 Latency is ${
+    //     Date.now() - new Date(interaction.createdTimestamp)
+    //   }ms. \n    API Latency is ${Math.round(client.ws.ping)}ms`
+    // );
   },
 };
