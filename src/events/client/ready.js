d4e4e7ef7310297e8841d896f5d8c802a1331700
Tommy Danks <tommydanks2000@hotmail.com>
2023-05-02 02:00:15 +0100

    presence

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,9 +1,20 @@
 const chalk = require("chalk");
+const { Client, ActivityType } = require("discord.js");
+const { status } = require("minecraft-server-util");
 
 module.exports = {
   name: "ready",
   once: true,
+  /**
+   *
+   * @param {Client} client
+   */
   async execute(client) {
+    await client.user.setPresence({
+      activities: [{ name: "YOU!", type: ActivityType.Watching }],
+      status: "dnd",
+    });
+
     console.log(chalk.green`[Z!NC] Ready! Logged in as ${client.user.tag}`);
   },
 };
