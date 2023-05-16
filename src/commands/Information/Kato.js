02ab863e99be21f5fd7023bdc79939a757e5fe20
Tommy <tommydanks2000@gmail.com>
2023-05-17 00:09:31 +0100

    new kato embed

--- src/commands/Information/Kato.js
+++ src/commands/Information/Kato.js
@@ -6,11 +6,22 @@ module.exports = {
     .setDescription("use me and find out"),
   async execute(interaction, client) {
     const embed = new EmbedBuilder()
-      .setTitle(`kato.to`)
+      .setTitle(
+        `Introducing Kato: Your Ultimate Destination for Anime Streaming`
+      )
       .setImage(`https://kato.to/iconn.png`)
       .setURL(`https://kato.to/`)
       .setDescription(
-        "Kato is the first free anime streaming website with Ai-Tools where you can watch English Subbed and Dubbed anime online. WATCH NOW! No Ads GUARANTEED! "
+        `
+        Are you a devoted anime enthusiast in search of a seamless streaming experience? Look no further than Kato! Unleashing the power of Ai-Tools, Kato revolutionizes your anime-watching journey, offering a vast library of English Subbed and Dubbed anime series. Immerse yourself in captivating storylines, vibrant characters, and enjoy a distraction-free streaming experience. Join the Anime Revolution - start streaming on Kato today!
+        
+        Features:
+          -English Subbed and Dubbed anime series
+          -Seamless and intuitive streaming experience
+          -Ai-Tools for enhanced convenience
+          -No ads guaranteed!
+          
+        Join Kato now and let your anime obsession soar to new heights!`
       )
       .setColor("#f1326d")
       .setFooter({
